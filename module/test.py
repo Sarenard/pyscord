e150b120af9223d9387df952b8972ba78088af60
Loris <monestierloris11@gmail.com>
2021-09-13 21:07:09 +0200

    update

--- module/test.py
+++ module/test.py
@@ -1,6 +1,6 @@
 import pyscord
 
-bot = pyscord.Client(token="ODc3NTY4Njc0NjY2NTI0Njgy.YR0hhA.qahcocZ66Ic7dQk8mgXLvwt4kK8")
+bot = pyscord.Client(token="ODc3NTY4Njc0NjY2NTI0Njgy.YR0hhA.qahcocZ66Ic7dQk8mgXLvwt4kK8", api_version=9)
 
 @pyscord.listener.event_message
 def on_message(message):
