00e05c98592a7e7452cb90c5c33755e65e0e1e53
Loris <monestierloris11@gmail.com>
2021-09-16 00:45:39 +0200

    update du channels.py pour avoir les nouveaux elements

--- module/channels.py
+++ module/channels.py
@@ -14,7 +14,34 @@ class Channel:
         self.raw = item
         self.item = item
         self.id = general.essai_element(item, "id")
+        self.type = general.essai_element(item, "type")
+        self.guild_id = general.essai_element(item, "guild_id")
+        self.position = general.essai_element(item, "position")
+        self.permission_overwrites = general.essai_element(item, "permission_overwrites")
         self.name = general.essai_element(item, "name")
+        self.topic = general.essai_element(item, "topic")
+        self.nsfw = general.essai_element(item, "nsfw")
+        self.last_message_id = general.essai_element(item, "last_message_id")
+        self.bitrate = general.essai_element(item, "bitrate")
+        self.user_limit = general.essai_element(item, "user_limit")
+        self.rate_limit_per_user = general.essai_element(item, "rate_limit_per_user")
+        try : self.recipients = [user.User(user) for user in general.essai_element(item, "dummy")]
+        except : self.recipients = None
+        self.icon = general.essai_element(item, "icon")
+        self.owner_id = general.essai_element(item, "owner_id")
+        self.application_id = general.essai_element(item, "application_id")
+        self.parent_id = general.essai_element(item, "parent_id")
+        self.last_pin_timestamp = general.essai_element(item, "last_pin_timestamp")
+        self.rtc_region = general.essai_element(item, "rtc_region")
+        self.video_quality_mode = general.essai_element(item, "video_quality_mode")
+        self.message_count = general.essai_element(item, "message_count")
+        self.member_count = general.essai_element(item, "member_count")
+        self.thread_metadata = general.essai_element(item, "thread_metadata")
+        self.member = general.essai_element(item, "member")
+        self.default_auto_archive_duration = general.essai_element(item, "default_auto_archive_duration")
+        self.permissions = general.essai_element(item, "permissions")
+
+
     def sendmessage(self, content, json={}):
         if json == {} : json = {"content" : content}
         url = f"https://discord.com/api/v{self.api_version}/channels/{self.id}/messages"
